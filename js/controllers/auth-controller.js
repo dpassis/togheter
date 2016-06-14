28915cf575c31f765bb8b00a4567503172586bac
dpassis <daniel@dpassis.com.br>
2016-06-14 17:19:18 -0300

    Delete old structure

--- js/controllers/auth-controller.js
+++
@@ -1,7 +0,0 @@
-angular.module('Togheter')
-.controller('AuthController',['$scope', '$routeParams', function ($scope, $routeParams){
-
-	$scope.name = "AuthController";
-    $scope.params = $routeParams;
-
-}]);
